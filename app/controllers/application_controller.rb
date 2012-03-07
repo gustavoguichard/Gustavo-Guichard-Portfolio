61ec6bb187aa4d36e048ba4157257ae1395369de
Gustavo Guichard <gustavoguichard@gmail.com>
2012-03-07 22:37:14 +0800

    Changed Application Controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   before_filter :is_admin
 
   def is_admin
-  	@site_settings = SiteSetting.first || nil
+  	if !@site_settings = SiteSetting.first then redirect_to new_site_settings_path end
   	if @site_settings.nil? then @admin
 	  else @admin = session[:nickname] == "gustavoguichard"
 	  end
