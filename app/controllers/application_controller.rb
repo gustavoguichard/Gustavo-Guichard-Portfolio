28f5449b825516710ce442687e6a0fe5f8d2ef82
Gustavo Guichard <gustavoguichard@gmail.com>
2012-12-15 12:20:58 -0200

    Cache site settings in session to avoid extra queries

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,12 @@ class ApplicationController < ActionController::Base
   before_filter :is_admin
 
   def is_admin
-    @site_settings = SiteSetting.first
+    @site_settings = session[:site_settings] || SiteSetting.first
     if @site_settings.nil? then
       @admin = true
       @site_settings = SiteSetting.new
     else
+      session[:site_settings] ||= @site_settings
       @admin = session[:nickname] == ENV['TWITTER_USERNAME']
     end
   end
