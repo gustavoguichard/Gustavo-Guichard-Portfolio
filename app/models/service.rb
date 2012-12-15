0cde4f0e2af80e8f33adea11ccb7f12318bd8332
Gustavo Guichard <gustavoguichard@gmail.com>
2012-12-15 14:56:08 -0200

    Parameterize URLS

--- app/models/service.rb
+++ app/models/service.rb
@@ -1,4 +1,8 @@
 class Service < ActiveRecord::Base
 	validates :title, :content, :image, :presence => true
   mount_uploader :image, ServiceUploader
+
+  def to_param
+    "#{id}-#{title.parameterize}"
+  end
 end
--- app/models/testimonial.rb
+++ app/models/testimonial.rb
@@ -1,4 +1,8 @@
 class Testimonial < ActiveRecord::Base
 	validates :name, :company, :content, :avatar, :presence => true
   mount_uploader :avatar, TestimonialUploader
+
+  def to_param
+    "#{id}-#{name.parameterize}"
+  end
 end
