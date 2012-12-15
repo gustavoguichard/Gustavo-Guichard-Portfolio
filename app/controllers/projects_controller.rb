9e1ba238b7187c93b6b38480fbde14b71e33528a
Gustavo Guichard <gustavoguichard@gmail.com>
2012-12-15 12:44:06 -0200

    Refactoring controllers

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,8 +1,6 @@
 class ProjectsController < ApplicationController
   respond_to :html, :json
 
-  # GET /projects
-  # GET /projects.json
   def index
     if params[:search].present?
       @projects = Project.search(params[:search]).includes(:tags)
@@ -19,8 +17,6 @@ class ProjectsController < ApplicationController
     end
   end
 
-  # GET /projects/new
-  # GET /projects/new.json
   def new
     if !@admin
       redirect_to root_path
@@ -29,8 +25,6 @@ class ProjectsController < ApplicationController
     end
   end
 
-  # POST /projects
-  # POST /projects.json
   def create
     @project = Project.new(params[:project])
     if @project.save && @admin
@@ -40,23 +34,18 @@ class ProjectsController < ApplicationController
     end
   end
 
-  # PUT /projects/1
-  # PUT /projects/1.json
   def update
     @project = Project.find_by_slug(params[:id])
     @project.update_attributes(params[:project]) if @admin
     respond_with @project
   end
 
-  # DELETE /projects/1
-  # DELETE /projects/1.json
   def destroy
     @project = Project.find(params[:id])
     @project.destroy if @admin
     redirect_to portfolio_url
   end
 
-  # POST /sort/  
   def sort
     params[:projects].each_with_index do |project, index|
       Project.update_all(['position=?', index+1], ['id=?', project[1]["id"]]) if @admin
