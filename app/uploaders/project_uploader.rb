86d04984ac8f634563bb7db0d0ec7256b9583759
Gustavo Guichard <gustavoguichard@gmail.com>
2013-10-12 22:41:33 -0700

    Resize to fix images to upload maximun width of 1024px and show on iOS

--- app/uploaders/project_uploader.rb
+++ app/uploaders/project_uploader.rb
@@ -1,7 +1,7 @@
 class ProjectUploader < CarrierWave::Uploader::Base
   include CarrierWave::RMagick
   storage :postgresql_lo
-  # process :resize_to_fit => [2400, 700]
+  process :resize_to_fit => [1024, 10000]
   def extension_white_list
     %w(jpg jpeg gif png)
   end
